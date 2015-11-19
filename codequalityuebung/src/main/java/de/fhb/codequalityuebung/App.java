bfdfc6bbf806a40b670e362287e538af772ccb93
mlevental <m.levental@web.de>
2015-11-19 20:55:30 +0100

    first commit

---
+++ codequalityuebung/src/main/java/de/fhb/codequalityuebung/App.java
@@ -0,0 +1,13 @@
+package de.fhb.codequalityuebung;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
